d7307ef2d1dbd76bb6127c462f1c90ce8b018e52
Eugene Chou <euchou@ucsc.edu>
2023-04-17 00:30:25 -0700

    adding more to the trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,50 @@
+/// A trait describing the basic functionality of a key management scheme.
 pub trait KeyManagementScheme {
+    /// The type containing anything required to initialize the key management scheme.
     type Init;
+    /// The type of a key.
     type Key;
-    type Id;
+    /// The type used to act as key identifiers (e.g., `u64` or `usize`).
+    type KeyId;
+    /// The associated error for fallible operations (e.g. `persist()`).
+    type Error;
 
     /// Sets up and returns the key management scheme.
     fn setup(init: Self::Init) -> Self;
 
-    /// Derive the key corresponding to `x`.
-    /// This key should not be kept around beyond any updates to `x`.
-    fn derive(&mut self, x: Self::Id) -> Self::Key;
+    /// Derive the key corresponding to the given `KeyId`. This key should not be kept beyond any
+    /// updates to that `KeyId`.
+    fn derive(&mut self, key: Self::KeyId) -> Self::Key;
 
-    /// Update the key corresponding to `x`.
-    /// The old key is truly underivable from `self` after calling `epoch()`.
-    fn update(&mut self, x: Self::Id) -> Self::Key;
+    /// Update the key corresponding to the given `KeyId`. The actual revocation of the old key can
+    /// be deferred, but must be guaranteed after calling `commit()`.
+    fn update(&mut self, key: Self::KeyId) -> Self::Key;
 
-    /// Makes every updated key truly underivable from `self`.
-    fn epoch(&mut self);
+    /// Commits any deferred key updates, making all udpated keys truly underivable from `self`.
+    fn commit(&mut self);
+
+    /// Compacts the internal state of `self`.
+    fn compact(&mut self);
+
+    /// Persists state to some writable location.
+    fn persist<W>(&self, location: W) -> Result<(), Self::Error>
+    where
+        W: std::io::Write;
 }
+
+/// A marker trait used to indicate that a `KeyManagementScheme` implementation is secure.
+/// Essentially, this should guarantee that there is only a negligible probability of being able to
+/// recover a key that has been updated.
+pub trait SecureKeyManagementScheme: KeyManagementScheme {}
+
+/// A marker trait used to indicate that a `KeyManagementScheme` implementation performs
+/// fine-grained updates: `update()` only affects the target key.
+pub trait FineGrainedKeyManagementScheme: KeyManagementScheme {}
+
+/// A marker trait used to indicate that a `KeyManagementScheme` implementation performs
+/// coarse-grained updates: `update()` may affect more keys than just the target key.
+pub trait CoarseGrainedKeyManagementScheme: KeyManagementScheme {}
+
+/// A marker trait used to indicate that a `KeyManagementScheme` implementation performs
+/// deferred updates: `update()` should be followed by `commit()` for true revocation.
+pub trait DeferredKeyManagementScheme: KeyManagementScheme {}
