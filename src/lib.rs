3b049e395c4cb59b21413111be68daadf3a5e78b
Eugene Chou <euchou@ucsc.edu>
2023-11-12 16:13:40 -0800

    add `Debug` bound

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub trait KeyManagementScheme {
     /// The type used to act as key identifiers.
     type KeyId;
     /// The associated error for fallible operations (e.g. `persist()`).
-    type Error: std::error::Error;
+    type Error: std::error::Error + core::fmt::Debug;
 
     /// Derive the key corresponding to the given `KeyId`.
     ///
